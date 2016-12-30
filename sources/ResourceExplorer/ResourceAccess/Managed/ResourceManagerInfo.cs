d675b7b34941503eda941349c322e632e79ec57d
James William Turner <james_w_turner27@hotmail.com>
2016-12-30 08:28:24 -0500

    added missing files

---
+++ sources/ResourceExplorer/ResourceAccess/Managed/ResourceManagerInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ResourceExplorer.ResourceAccess.Managed
+{
+    public class ResourceManagerInfo : ManagedResourceInfo
+    {
+        private ResourceManagerType _DesignerType;
+
+        public ResourceManagerType DesignerType
+        {
+            get { return _DesignerType; }
+        }
+
+        public ResourceManagerInfo(ModuleInfo module, string name, ResourceManagerType designerType) 
+            : base(module, ManagedResourceType.ResourceManager, name)
+        {
+            _DesignerType = designerType;
+        }
+    }
+}
---
+++ sources/ResourceExplorer/ResourceAccess/Managed/ResourceManagerType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ResourceExplorer.ResourceAccess.Managed
+{
+    public enum ResourceManagerType
+    {
+        Form,
+        Control,
+        Component,
+        Project,
+        Other
+    }
+}
